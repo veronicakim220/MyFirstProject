bc666798b6d1a5ea154ee3a9f8aac4f7f5a98b3a
Ji Won (Veronica) <veronicakim220@gmail.com>
2013-09-22 15:24:22 -0400

    First Revision of my first program

---
+++ myfirstprogram.py
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+#
+#Programmed by: Ji Won (Veronica Kim)
+#Date: Sept
+#
+#
+
+i = 0
+j = 2
+if (i == 2 and j == 2):
+    print ("it works")
+elif (i == 0 and j == 0):
+    print ("it's not 2")
+
+else:
+    input ("not good enough, input another number \n")
+
+if ( j > 0):
+    print ("eh, do better next time")
